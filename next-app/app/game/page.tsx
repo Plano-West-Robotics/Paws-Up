582d31bcd2110d97dca963b75b774cd3b834e2e1
Aryan Bangad <aryanparagbangad@gmail.com>
2023-12-27 13:42:12 -0600

    updating comments cuz deployment hate me

--- next-app/app/game/page.tsx
+++ next-app/app/game/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TermDisplay from '../components/TermDisplay';
 
+// change the 1 with the id of the selected list but should be good for the purposes of demonstration
+
 const YourPage = () => {
   return (
     <div>
       <h1>Your Page</h1>
-      <TermDisplay termSetId = {1} /> // change the 1 with the id of the selected list but should be good for the purposes of demonstration
+      <TermDisplay termSetId = {1} /> 
     </div>
   );
 };
