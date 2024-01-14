dced276a78840e895fe607cc16e04e2779e7449e
Aryan Bangad <aryanparagbangad@gmail.com>
2024-01-14 01:38:16 -0600

    more!

--- next-app/app/ladder/page.js
+++ next-app/app/ladder/page.js
@@ -1,5 +1,10 @@
 export default function Home() {
     return (
-        <div className="flex-grow text-center text-xl font-bold mt-5">Sathvik on TOP!!</div>
+        <div className=" flex - row flex-grow text-center text-xl font-bold mt-5">
+            <div>Sathvik on TOP!!
+            </div>
+            <div>ELO: 3000</div>
+            <div>RANK: 1</div>
+        </div>
     )
 };
