7fa2c2a46a1038bd899cdd337858bc82bf75c348
Aryan Bangad <aryanparagbangad@gmail.com>
2024-02-08 23:11:35 -0600

    data added seed.mjs save.

--- next-app/prisma/seed.mjs
+++ next-app/prisma/seed.mjs
@@ -5,19 +5,21 @@ const prisma = new PrismaClient();
 
 async function main() {
     for (let termsSet of termsSets) {
+        // Convert the standard field to a string
+        termsSet.standard = termsSet.standard.toString();
+
+        // Add the subject field with the value "English"
+        termsSet.subject = "Science";
+
         await prisma.termsSet.create({
-            data:termsSet
-        })
+            data: termsSet
+        });
     }
-    
 }
 
 main().catch(e => {
     console.log(e);
-    process.exit(1)
+    process.exit(1);
 }).finally(() => {
     prisma.$disconnect();
-})
-
-
-
+});
